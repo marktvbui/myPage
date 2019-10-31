a4840bfe77f06395b3c3abbe45c8e9ef09ae62d6
Mark Bui <mark.bui24@gmail.com>
2019-10-30 21:09:26 -0700

    commented out js file

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,18 +1,18 @@
 $(document).ready(function() {
   console.log("yo yo yo");
-  function onSignIn(googleUser) {
-    console.log("sign on app.js");
-    var profile = googleUser.getBasicProfile();
-    console.log("ID: " + profile.getId()); // Do not send to your backend! Use an ID token instead.
-    console.log("Name: " + profile.getName());
-    console.log("Image URL: " + profile.getImageUrl());
-    console.log("Email: " + profile.getEmail()); // This is null if the 'email' scope is not present.
-  }
-  function signOut() {
-    console.log("sign out");
-    var auth2 = gapi.auth2.getAuthInstance();
-    auth2.signOut().then(function() {
-      console.log("User signed out.");
-    });
-  }
+  //   function onSignIn(googleUser) {
+  //     console.log("sign on app.js");
+  //     var profile = googleUser.getBasicProfile();
+  //     console.log("ID: " + profile.getId()); // Do not send to your backend! Use an ID token instead.
+  //     console.log("Name: " + profile.getName());
+  //     console.log("Image URL: " + profile.getImageUrl());
+  //     console.log("Email: " + profile.getEmail()); // This is null if the 'email' scope is not present.
+  //   }
+  //   function signOut() {
+  //     console.log("sign out");
+  //     var auth2 = gapi.auth2.getAuthInstance();
+  //     auth2.signOut().then(function() {
+  //       console.log("User signed out.");
+  //     });
+  //   }
 });
