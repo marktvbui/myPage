65c648c84d44bbb38ca2281d635822ee582c72a8
Mark Bui <mark.bui24@gmail.com>
2019-10-30 18:34:35 -0700

    testing

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,12 @@
 $(document).ready(function() {
+  function onSignIn(googleUser) {
+    console.log("sign on app.js");
+    var profile = googleUser.getBasicProfile();
+    console.log("ID: " + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    console.log("Name: " + profile.getName());
+    console.log("Image URL: " + profile.getImageUrl());
+    console.log("Email: " + profile.getEmail()); // This is null if the 'email' scope is not present.
+  }
   function signOut() {
     var auth2 = gapi.auth2.getAuthInstance();
     auth2.signOut().then(function() {
