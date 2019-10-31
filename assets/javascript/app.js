0481cbc0c2149ed0893bf125ae02c8f6b45f47c6
Mark Bui <mark.bui24@gmail.com>
2019-10-30 18:27:27 -0700

    moving signin function to html page

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,13 +1,4 @@
 $(document).ready(function() {
-  function onSignIn(googleUser) {
-    console.log("sign on");
-    var profile = googleUser.getBasicProfile();
-    console.log("ID: " + profile.getId()); // Do not send to your backend! Use an ID token instead.
-    console.log("Name: " + profile.getName());
-    console.log("Image URL: " + profile.getImageUrl());
-    console.log("Email: " + profile.getEmail()); // This is null if the 'email' scope is not present.
-  }
-
   function signOut() {
     var auth2 = gapi.auth2.getAuthInstance();
     auth2.signOut().then(function() {
